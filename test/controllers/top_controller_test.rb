7ba836cdc3c95cf5322d0113b84be721f9b7d32d
httrhtm <h_hattori@emyus.jp>
2021-07-29 16:55:25 +0900

    deveiceを用いてユーザー管理ファイルを生成

--- test/controllers/top_controller_test.rb
+++ test/controllers/top_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
-class LoginControllerTest < ActionDispatch::IntegrationTest
+class TopControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get login_index_url
+    get top_index_url
     assert_response :success
   end
 end
