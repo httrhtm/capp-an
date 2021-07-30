1064ca2b8f258311b932619d92e80b2342d0cd81
httrhtm <h_hattori@emyus.jp>
2021-07-30 14:45:07 +0900

    ストロングパラメーターを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  protected
+ 
+  # username フィールドを許可する strong parameters の設定
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
 end
